9f120caee8b98f6d604c9b76f18840364541713f
shaikhazam990 <shaikhazam0990@gmail.com>
2026-05-07 01:35:45 +0530

    create image

---
+++ Backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+app.use(express.json());
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  let sum =0;
+    for(let i=0; i<1000000000; i++){
+        sum+=i;
+    }
+  res.send(`Hello World! Sum: ${sum}`);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}); 
+
+
